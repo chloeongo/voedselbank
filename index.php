847872fb5ea35bade158d7a8c1c0b81b54dc2b42
chloeongo <chloe.ongolesono@gmail.com>
2025-09-25 13:04:54 +0200

    home dashboard toegevoegd
    verbeteringen moeten nog gemaakt worden

--- index.php
+++ index.php
@@ -44,11 +44,37 @@
                 <img src="./styles/images/icon-beheer.png">
                 <a href="index.php">Beheren</a>
             </div>
-
         </div>
       </header>
      
       <main>
+            <h2>Welkom &lt;Gebruiker&gt;</h2>
+
+        <div class="dashboard">
+            <div class="card">
+                <h3><a href="#">Leveringen</a></h3>
+                <div class="item"><a href="#">Leverancier naam - 10/09/2025</a></div>
+                <div class="item"><a href="#">Leverancier naam - 11/09/2025</a></div>
+                <div class="item"><a href="#">Leverancier naam - 12/09/2025</a></div>
+                <a href="#" class="button-link">Beheer leveranciers</a>
+            </div>
+
+            <div class="card">
+                <h3><a href="#">Voorraad</a></h3>
+                <div class="item"><a href="#">Product naam &lt;123456&gt;</a></div>
+                <div class="item"><a href="#">Product naam &lt;123456&gt;</a></div>
+                <div class="item"><a href="#">Product naam &lt;123456&gt;</a></div>
+                <a href="#" class="button-link">Beheer voorraad</a>
+            </div>
+
+            <div class="card">
+                <h3><a href="#">Klanten</a></h3>
+                <div class="item"><a href="#">Familie naam</a></div>
+                <div class="item"><a href="#">Familie naam</a></div>
+                <div class="item"><a href="#">Familie naam</a></div>
+                <a href="#" class="button-link">Beheer klanten</a>
+            </div>
+        </div>
 
       </main>
     </section>
