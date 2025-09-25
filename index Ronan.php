4ca922ecfa23914b6e1349d12883edac7770217c
Roemang <157353477+Roemang@users.noreply.github.com>
2025-09-25 12:55:24 +0200

    Add files via upload

---
+++ index Ronan.php
@@ -0,0 +1,41 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="index CSS Ronan.css">
+</head>
+<body>
+
+    <h2>Welkom &lt;Gebruiker&gt;</h2>
+
+    <div class="dashboard">
+        <div class="card">
+            <h3><a href="#">Leveringen</a></h3>
+            <div class="item"><a href="#">Leverancier naam - 10/09/2025</a></div>
+            <div class="item"><a href="#">Leverancier naam - 11/09/2025</a></div>
+            <div class="item"><a href="#">Leverancier naam - 12/09/2025</a></div>
+            <a href="#" class="button-link">Beheer leveranciers</a>
+        </div>
+
+        <div class="card">
+            <h3><a href="#">Voorraad</a></h3>
+            <div class="item"><a href="#">Product naam &lt;123456&gt;</a></div>
+            <div class="item"><a href="#">Product naam &lt;123456&gt;</a></div>
+            <div class="item"><a href="#">Product naam &lt;123456&gt;</a></div>
+            <a href="#" class="button-link">Beheer voorraad</a>
+        </div>
+
+        <div class="card">
+            <h3><a href="#">Klanten</a></h3>
+            <div class="item"><a href="#">Familie naam</a></div>
+            <div class="item"><a href="#">Familie naam</a></div>
+            <div class="item"><a href="#">Familie naam</a></div>
+            <a href="#" class="button-link">Beheer klanten</a>
+        </div>
+    </div>
+
+</body>
+</html>
