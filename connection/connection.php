21af0bc23acfd0f5d21d17a057f7ae3c2ee3adbe
chloeongo <chloe.ongolesono@gmail.com>
2025-09-29 12:20:09 +0200

    database connectie gemaakt

---
+++ connection/connection.php
@@ -0,0 +1,23 @@
+<?php
+$pdo = dbConnect();
+
+function dbConnect()
+{
+    $servername = "127.0.0.1:3307";
+    $username = "root";
+    $password = "";
+    $dbname = "voedselbank";
+
+    try {
+        $dsn = "mysql:host=$servername;dbname=$dbname;port=3307";
+        $pdo = new PDO($dsn, $username, $password);
+
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, true);
+        return $pdo;
+    } catch (PDOException $e) {
+        echo "Connection failed: " . $e->getMessage() . "\n";
+        return null;
+    }
+}
+?>
