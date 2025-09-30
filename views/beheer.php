b902d85379f6ed65abe1464512f2d9cd0e0f7c73
chloeongo <chloe.ongolesono@gmail.com>
2025-09-30 16:12:54 +0200

    gebruikerform front end gemaakt

--- views/beheer.php
+++ views/beheer.php
@@ -8,7 +8,7 @@ $pdo = dbConnect();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Voedselbank Maaskantje</title>
+    <title>Beheren</title>
     <link rel="icon" type="image/x-icon" href="../styles/images/logo.png">
     <link rel="stylesheet" href="../styles/styles.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
@@ -110,7 +110,7 @@ $pdo = dbConnect();
 
                 </div>
                 <div class="addBtn">
-                    <a href="/#">Voeg nieuwe gebruikers toe</a>
+                    <a href="gebruikerform.php">Voeg nieuwe gebruikers toe</a>
                 </div>
             </div>
         </div>
