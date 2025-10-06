d14f02027a44de97925872b80f0ba2f6224993eb
Roemang <157353477+Roemang@users.noreply.github.com>
2025-10-06 13:31:01 +0200

    Create leverancier-toevoegen.php

---
+++ leverancier-toevoegen.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Product toevoegen - Voedselbank Maaskantje</title>
+  <link rel="icon" type="image/x-icon" href="./styles/images/logo.png">
+  <link rel="stylesheet" href="./styles/product-toevoegen.css">
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+</head>
+<body class="bodyLayout">
+  <section>
+    <header>
+      <div id="headerImg">
+          <img src="./styles/images/logo.png" alt="logo">
+      </div>
+
+      <div id="nav">
+          <div class="navLink">
+              <img src="./styles/images/icon-home.png" alt="">
+              <a href="index.php">Home</a>
+          </div>
+          <div class="navLink">
+              <img src="./styles/images/icon-user.png" alt="">
+              <a href="index.php">Mijn account</a>
+          </div>
+          <div class="navLink">
+              <img src="./styles/images/icon-leverancier.png" alt="">
+              <a href="index.php">Leveranciers</a>
+          </div>
+          <div class="navLink active">
+              <img src="./styles/images/icon-voorraad.png" alt="">
+              <a href="voorraad.php">Voorraad</a>
+          </div>
+      </div>
+    </header>
+
+    <main class="content">
+      <div class="form-wrapper">
+        <a href="leveranciers.php" class="back-btn">Ga terug</a>
+
+        <div class="form-container">
+          <h2>Voeg een leverancier toe</h2>
+          <form action="#" method="post">
+            <label for="bedrijfsnaam">Bedrijfsnaam:</label>
+            <input type="text" id="bedrijfsnaam" name="bedrijfsnaam" placeholder="Bedrijfsnaam" required>
+
+            <label for="adres">Adres:</label>
+            <input type="text" id="adres" name="adres" placeholder="Adres" required>
+
+            <label for="contact persoon">Contact persoon:</label>
+            <input type="number" id="contact persoon" name="contact persoon" placeholder="Contact Persoon" required>
+
+            <label for="e-mail">E-Mail:</label>
+            <input type="text" id="e-mail" name="categorie" placeholder="E-Mail" required>
+
+            <button type="submit" class="btn-submit">Voeg toe</button>
+          </form>
+        </div>
+      </div>
+    </main>
+  </section>
+</body>
+</html>
